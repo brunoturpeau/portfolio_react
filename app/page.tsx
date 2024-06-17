b128b7d90558157d40765c9fd64d3bee39b45a03
Bruno Turpeau <brunoturpeau99@gmail.com>
2024-06-17 22:04:58 +0200

    feat: add coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
       <div className={`h-full flex flex-col sm:flex-row`}>
         <Navigation />
-        <main className={`flex flex-col items-center w-full h-screen w-full justify-center`}>
+        <main className={`flex flex-col items-center h-screen w-full justify-center`}>
           <h1>Bruno Turpeau</h1>
             <h2>Développeur web</h2>
+            <p className={`mt-5 italic`}>Coming soon ...</p>
         </main>
       </div>
   )
